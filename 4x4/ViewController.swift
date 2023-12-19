8127296521671414a4e73d8227516a35b3ea1484
ZhandosXIX <ospanbekov.k@mail.ru>
2023-12-19 21:11:29 +0600

    Initial Commit

---
+++ 4x4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4x4
+//
+//  Created by zhandos on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4x4UITests/_x4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _x4UITestsLaunchTests.swift
+//  4x4UITests
+//
+//  Created by zhandos on 19.12.2023.
+//
+
+import XCTest
+
+final class _x4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
